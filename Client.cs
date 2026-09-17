4519d89b126f1fa5278d4552dad7ab4f13c1fd81
Jeremy <jeremy.frank@students.snow.edu>
2026-09-16 21:17:43 -0600

    Complete client domain model assignment

---
+++ Client.cs
@@ -0,0 +1,54 @@
+﻿namespace CompanyManagement.Models
+{
+    public class Client
+    {
+        public Guid Id;
+
+        public string Name;
+
+        public string Email;
+
+        public DateTime CreatedOn;
+
+        private bool IsActive;
+
+        // Creates a new client with a unique ID and current date
+        public Client(string name, string email)
+        {
+
+            Name = name;
+            Email = email;
+            Id = Guid.NewGuid();
+            CreatedOn = DateTime.Now;
+            IsActive = true;
+        }
+
+        // Used when creating a client with existing information
+        public Client(Guid id, string name, string email, DateTime createdOn, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            CreatedOn = createdOn;
+            IsActive = isActive;
+        }
+        public string GetSummary()
+        {
+            string status;
+
+            if (IsActive)
+              status = "Active";
+            else
+                status = "Inactive";
+
+            return $"Client ID: {Id}\nName: {Name}\nEmail: {Email}\nCreated On: {CreatedOn}\nStatus: {status}";
+        }
+
+
+        // Changes the current client's status without creating a new Client
+        public void Deactivate()
+        {
+            IsActive = false;
+        }
+    }       
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using CompanyManagement.Models;
+
+public class Program
+{
+    static void Main (string[] args)
+    {
+        // Create two separate Client objects
+        Client myclient1 = new Client("Jeremy", "jeremy.frank@students.snow.edu");
+        Client myclient2 = new Client("Anna", "annaisacoolmom@gmail.com");
+
+
+        // Deactivate only the first client
+        myclient1.Deactivate();
+
+        Console.WriteLine(myclient1.GetSummary());
+
+        Console.WriteLine(myclient2.GetSummary());
+        
+    }
+
+    
+}
